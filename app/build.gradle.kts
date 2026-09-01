738d9281cfaece267c0d8f38a30bd3342e5c9cb2
neo <neo@Mac.lan>
2026-09-02 00:01:26 +0700

    bump: version 2.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.itsazni.notificationforwarder"
         minSdk = 26
         targetSdk = 36
-        versionCode = 212
-        versionName = "2.1.2"
+        versionCode = 213
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
